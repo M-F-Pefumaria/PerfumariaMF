9163b2bdff58da0e3e51685c0fdfa884de2dc1dd
Rhuan <rhuansantos.cp@gmail.com>
2025-11-07 13:54:51 -0300

    feat: Configurei o Express e a conexão com o Banco de Dados

---
+++ src/config/database.js
@@ -0,0 +1,25 @@
+const mysql = require('mysql2');
+
+require('dotenv').config();
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+pool.getConnection((err, connection) => {
+    if (err) {
+        console.error('Erro ao conectar ao banco:', err.message);
+        return;
+    }
+    console.log('Conectado ao banco de dados MySQL!');
+    connection.release();
+});
+
+
+module.exports = pool.promise();
